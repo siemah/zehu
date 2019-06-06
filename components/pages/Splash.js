55e112ea5456085f0aee9705d50f431cb9777706
siemah <m_saidouni@inptic.edu.dz>
2019-06-06 05:28:43 +0200

    (Build)add documentation to Splash

--- components/pages/Splash.js
+++ components/pages/Splash.js
@@ -3,8 +3,13 @@ import { StyleSheet, View, Animated, Image } from 'react-native'
 import { Container, Content } from 'native-base';
 
 import splashStyle from '../../assets/styles/splash';
-console.warn("object", splashStyle.center)
 
+/**
+ * Splash screen
+ * @author siemah
+ * @version 1.0.0
+ * @param {Object} props contain a list of propreties
+ */
 const Splash = props => (
   <Container>
     <View style={[ StyleSheet.absoluteFillObject, splashStyle.center ]}>
