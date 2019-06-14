7e7dc37a0d3033cbde0ba427c5aad939c7bc0c02
siemah <m_saidouni@inptic.edu.dz>
2019-06-14 04:01:06 +0200

    (Build)Replacing ScrollView by FlatList at VerticalCard

--- components/uis/VerticalCard.js
+++ components/uis/VerticalCard.js
@@ -1,49 +1,55 @@
 import React from 'react'
-import { StyleSheet, ActivityIndicator } from 'react-native';
-import { View, Text, Content, Body, Left, Thumbnail, List, ListItem } from 'native-base'
+import { StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { View, Text, Content, Body, Left, Thumbnail, List, ListItem, Icon } from 'native-base'
 
 import ScrollThumbnail from './ScrollThumbnail';
 
 const removeHtmlTags = html => html.replace(/<\/?[p|div|em|del|strong|b|u|i]>/gi, "");
 const VerticalCard = ({ data, goTo=null, onPress=null }) => {
   const { loading, articles } = data;
+  console.warn('artices on flat', articles);
+  
   return (
     <Content>
       {/** The text container left side */}
+      
       <List
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}>
         { onPress && <ScrollThumbnail onPress={onPress} /> }
         {
-          loading ?
-            (<ActivityIndicator size='large' color='#0e1636' style={styles.activityIndicator} />) :
-            (
-              articles.map(article => (
-                <ListItem key={article.id} thumbnail noIndent noBorder={true} onPress={() => {
-                  goTo('Article', article)
-                }}>
-                  <Left style={{ elevation: 15, backgroundColor: 'rgba(255,255,255, 0.005)' }}>
-                    <Thumbnail source={{ uri: article.image.url }} style={styles.thumbnail} square large />
-                  </Left>
-                  <Body>
-                    <View>
-                      <Text numberOfLines={2} style={styles.articleTitle}>{article.title}</Text>
-                    </View>
-                    <View>
-                      <Text numberOfLines={2} style={styles.articleExtract}>
-                        { removeHtmlTags(article.summary) }
-                      </Text>
-                    </View>
-                    {/** here maybe will add some like and stuffs like that */}
-                  </Body>
-                  <View style={styles.divider}></View>
-                </ListItem>
-              ))
+          loading 
+            ? <ActivityIndicator size='large' color='blue' />
+            : (
+              <FlatList
+                data={articles}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item: article }) => (
+                  <ListItem key={article.id} thumbnail noIndent noBorder={true} onPress={() => {
+                    goTo('Article', article)
+                   }}>
+                    <Left style={{ elevation: 15, backgroundColor: 'rgba(255,255,255, 0.005)' }}>
+                      <Thumbnail source={{ uri: article.image.url }} style={styles.thumbnail} square large />
+                    </Left>
+                    <Body>
+                      <View>
+                        <Text numberOfLines={2} style={styles.articleTitle}>{article.title}</Text>
+                      </View>
+                      <View>
+                        <Text numberOfLines={2} style={styles.articleExtract}>
+                          {removeHtmlTags(article.summary)}
+                        </Text>
+                      </View>
+                      {/** here maybe will add some like and stuffs like that */}
+                    </Body>
+                    <View style={styles.divider}></View>
+                  </ListItem>
+                )}
+              />
             )
         }
 
       </List>
-      <View></View>
     </Content>
   )
 }
