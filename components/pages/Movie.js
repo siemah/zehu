ef72aed36179ab0e628758185fc896896f5934e8
siemah <m_saidouni@inptic.edu.dz>
2019-07-01 17:09:35 +0200

    (Build)Fetch extas data of current movie

--- components/pages/Movie.js
+++ components/pages/Movie.js
@@ -6,72 +6,93 @@ import { Content, Button, Icon, Left, Right, Body, H1, H3, Row, Col, } from 'nat
 import Axios from 'axios';
 
 const { height, width } = Dimensions.get('screen'); 
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = "28ecfb177a1845471436402e21e6f977";
+/**
+ * @name useMovie
+ * is a custom hook to retrieve some extra details about the current movie   
+ * @param {String} movieId the id of movie use to get some details about it
+ * @return [movie] details returned by the tmdb api
+ */
+const useMovie = movieId => {
+  const [movie, setMovie] = useState(null);
+  let _isMount = true;
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      try {
+        let { data: movie } = await Axios.get(`${apiUrl}/movie/${movieId}?api_key=${apiKey}`);
+        _isMount && setMovie(movie);
+      } catch (error) {
+        _isMount && setMovie(null);
+        console.warn(error);
+      }
+    }
+    fetchMovieData();
+    return () => {
+      _isMount = false;
+    };
+  }, []);
+  return [movie,];
+}
 
 const Movie = ({ navigation=null }) => {
-  /*let { title, source, id, vote_average, video, overview } = navigation.state.params;
-    current movie has: release_date, genres: [{id, name}], production_countries: [{id, name}], release_date, runtime(movies duration)
+  let { title, source, id, vote_average, video, overview } = navigation.state.params;
+  /* current movie has: release_date, genres: [{id, name}], production_countries: [{id, name}], runtime(movies duration)
   */
+  const [ movie=null ] = useMovie(id);
+  
   
   return (
     <Content style={styles.container}>
       <View style={styles.headerContainer}>
-        <ImageBackground source={require('../../assets/images/movies.jpg')} style={styles.movieImage} />
+        <ImageBackground source={source} style={styles.movieImage} />
         <View style={styles.overlay} />
         <View style={styles.headerActionsContainer}>
           <HeaderIcon />
         </View>
       </View>
       <View style={styles.contentActionsContainer}>
-          <Left>
-            <Icon name='md-add' style={styles.iconStyle} />
-          </Left>
-          <Body>
-            <Button style={styles.playiconContainer} transparent onPress={alert}>
-              <Icon name='md-play' style={styles.iconStyle} />
-            </Button>
-          </Body>
-          <Right>
-            <Icon name='md-share' style={styles.iconStyle} />
-          </Right>
+        <Left>
+          <Icon name='md-add' style={styles.iconStyle} />
+        </Left>
+        <Body>
+          <Button style={styles.playiconContainer} transparent onPress={alert}>
+            <Icon name='md-play' style={styles.iconStyle} />
+          </Button>
+        </Body>
+        <Right>
+          <Icon name='md-share' style={styles.iconStyle} />
+        </Right>
       </View>
       <View style={[styles.center,]}>
         <View style={[styles.center]}>
-          <H1>Title</H1>
+          <H1>{title}</H1>
         </View>
-        <View style={[styles.center]}>
-          <Text>Category, Comedy, Drame</Text>
-        </View>
+        {
+          movie && (
+            <View style={[styles.center]}>
+              <Text>{ movie.genres.map(({ name }) =>  name).join(', ') }</Text>
+            </View>
+          )
+        }
         <View><H1>Rating Component</H1></View>
         <Row style={[styles.row,]}>
           <Col style={[styles.center]}>
             <Text>Rate</Text>
-            <H3 style={styles.color}>9/10</H3>
+            <H3 style={styles.color}>{vote_average}/10</H3>
           </Col>
           <Col style={[styles.center,]}>
             <Text>Country</Text>
-            <H3 style={styles.color}>USA</H3>
+            {movie && <H3 style={[styles.color, { fontSize: 11}]} numberOfLines={1}>{ movie.production_countries[0].name }</H3>}
           </Col>
           <Col style={[styles.center,]}>
             <Text>Duration</Text>
-            <H3 style={styles.color}>123min</H3>
+            {movie && <H3 style={styles.color}>{`${movie.runtime}min`}</H3>}
           </Col>
         </Row>
         <View>
           <Text style={{...styles.color, ...styles.center, padding: 15, }}>
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            Overview of the curren movie, it's kind like a description of this movie
-            and talk aboutn scenario and 
+            { `\t` + overview }
           </Text>
         </View>
       </View> 
