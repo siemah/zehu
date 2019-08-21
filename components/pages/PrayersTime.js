74a1749c6a8b6b78da5348b3abc178c8f870408a
siemah <m_hamiche@inptic.edu.dz>
2019-08-21 12:51:42 +0100

    get current location of user using device location

--- components/pages/PrayersTime.js
+++ components/pages/PrayersTime.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { Content, Text } from 'native-base';
+import Axios from 'axios';
+
+const link = `https://api.pray.zone/v2/times/this_week.json?elevation=8000&school=8`;
+/**
+ * 1- if yes, then update the state and save coords on AsyncStorage
+ * 2- if not then use a city name rather than location coords
+ */
+const getCoords = config => new Promise((resolve, reject) => {
+  navigator.geolocation.getCurrentPosition(({coords}) => {
+    resolve(coords);
+  }, err => reject(err), config);
+})
 
 const PrayersTime = () => {
-  
+
   return (
     <Content>
-      <Text>PrayersTime</Text>
+      <Text>prayers times </Text>
     </Content>
   )
 }
 
-export default PrayersTime
+export default PrayersTime;
