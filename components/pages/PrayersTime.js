d35884a6afddce86e35227fd8988ab8e22ad1377
siemah <m_hamiche@inptic.edu.dz>
2019-08-21 12:55:43 +0100

    retrieve prayers time hook using coords

--- components/pages/PrayersTime.js
+++ components/pages/PrayersTime.js
@@ -12,12 +12,49 @@ const getCoords = config => new Promise((resolve, reject) => {
     resolve(coords);
   }, err => reject(err), config);
 })
-
+/**
+ * 1- verify if the user has enable reaching the location using devices
+ * 2- fetch the data for this mounth
+ * 3- update the state and render the list of times
+ * 4- cleaning fetch request
+*/
+const usePrayersTimes = (city=null) => {
+  const _isMounted = true;
+  const [prayersTime, setPrayersTime] = useState(null);
+  const CancelToken = Axios.CancelToken;
+  const source = CancelToken.source();
+  //if(!city && !coords) return [null];
+  useEffect(() => {
+    const fetchPrayers = async () => {
+      try {
+        let coords = await getCoords();
+        console.warn(coords);
+        let { data, status } = await Axios.get(`${link}&longitude=${coords.longitude}&latitude=${coords.latitude}`, {
+          cancelToken: source.token
+        });
+        if(status === 200 && data.status === 'OK') {
+          _isMounted && setPrayersTime(data.results);
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    }
+    fetchPrayers();
+    return () => {
+      _isMounted = false;
+      // canceling the reauest
+      source.cancel('Operation canceled by the user.');
+    };
+  }, []);
+  return [ prayersTime, setPrayersTime]
+;}
 const PrayersTime = () => {
+  
+  const [prayersTimes] = usePrayersTimes();
 
   return (
     <Content>
-      <Text>prayers times </Text>
+      <Text>prayers times {JSON.stringify(prayersTimes)}</Text>
     </Content>
   )
 }
